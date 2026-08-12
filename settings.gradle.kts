0ab19c728ef544eb40315554290277141d8e73c2
sannanornberg-dev <sannanornberg@gmail.com>
2026-08-12 12:40:10 +0000

    Add JitPack repository for whisper.cpp dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://jitpack.io") }
     }
 }
+
 rootProject.name = "TheHapticPath"
 include(":app")
